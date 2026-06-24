c22a5851aa4b4375cbe60a1b6093c613b115c615
anapaulanegrin <anapaulanegrin26@gmail.com>
2026-06-24 14:42:56 -0300

    Correcciones apis
    Correcciones permiso ruta cleanup y correccion formato api analytics

--- proyecto-c-buyer2-groovy-music-store/middleware.ts
+++ proyecto-c-buyer2-groovy-music-store/middleware.ts
@@ -46,7 +46,12 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    /*
+     * Excluye:
+     * - api/orders/cleanup 
+     * - _next (Archivos internos de Next.js)
+     * - Archivos estáticos (imágenes, favicons, etc)
+     */
+    '/((?!api/orders/cleanup|_next|.*\\..*).*)',
   ],
 };
